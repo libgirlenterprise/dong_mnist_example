42f5c9474932c119680ca5aca2157816a34bcbe7
Shaka Chen <scchen@libgirl.com>
2019-04-26 12:32:59 +0800

    save the trained model

--- tabemasu/__init__.py
+++ tabemasu/__init__.py
@@ -1,9 +1,10 @@
 
 
 import json
-
+import os
 import tabemasu.data
 import tabemasu.models.mlp
+import tabemasu.save_load
 import tabemasu.train
 
 
@@ -13,4 +14,6 @@ def main():
                                             train_data=tabemasu.data.get_train_data(),
                                             eval_data=tabemasu.data.get_eval_data(),
                                             config=json.load(f))
+        os.makedirs("/tmp/save_dir", exist_ok=True)
+        tabemasu.save_load.save(model, '/tmp/save_dir/')
         print(score)
---
+++ tabemasu/save_load.py
@@ -0,0 +1,5 @@
+def save(model, save_dir):
+
+    model.save(save_dir + 'my_model.h5')
+    pass
+
