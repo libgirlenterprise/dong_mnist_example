248c632c25d6aff65d89cd510f012d2c21da7507
Shaka Chen <scchen@libgirl.com>
2019-04-29 20:23:41 +0800

    endpoint user script should return a json

--- tabemasu/endpoint.py
+++ tabemasu/endpoint.py
@@ -6,4 +6,4 @@ import json, numpy
 def respond_to_request(model, request_body_json): # should support more formats
     data = json.loads(request_body_json)
     x = numpy.array(data) / 255.0
-    return model.predict(x)
+    return json.dumps(model.predict(x).tolist())
