4803b3333599d319f34cc91ce87e66c6b4388929
Shaka Chen <scchen@libgirl.com>
2019-05-09 11:22:58 +0800

    change version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tabemasu',
-      version='0.1',
+      version='0.2',
       description='none',
       license='apache2',
       install_requires=[
