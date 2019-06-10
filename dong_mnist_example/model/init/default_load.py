fba0483fdd82552af79425f7464146d7495ffd1e
Shaka Chen <scchen@libgirl.com>
2019-06-10 12:14:04 +0800

    move model deserialization code into DefaultModelLoad

--- dong_mnist_example/model/init/default_load.py
+++ dong_mnist_example/model/init/default_load.py
@@ -1,12 +1,15 @@
 import tensorflow
 from dong_mnist_example.model.init.default import DefaultModelInit
-
+from dong_mnist_example.data.default import DefaultData
 
 class DefaultModelLoad(DefaultModelInit):
 
-    def __init__(self, config={}, data_params=None, save_dir=None):
+    def __init__(self, config={}, save_dir=None):
 
-        super().__init__(config, data_params, save_dir)
+        data = DefaultData()
+        super().__init__(config,
+                         data_params=data.get_data_params(),
+                         save_dir=save_dir)
         model = tensorflow.keras.models.load_model(save_dir + 'my_model.h5',
                                                    custom_objects={
                                                        'DefaultTrainModel': tensorflow.keras.models.Sequential
--- dong_mnist_example/service/default.py
+++ dong_mnist_example/service/default.py
@@ -4,14 +4,10 @@ from __future__ import (absolute_import, division, print_function,
 import json, numpy
 import dong.framework
 from dong_mnist_example.model.init.default_load import DefaultModelLoad
-from dong_mnist_example.data.default import DefaultData
+
 
 class DefaultService(DefaultModelLoad, dong.framework.Service):
 
-    def __init__(self, save_dir):
-        data = DefaultData()
-        super().__init__(data_params=data.get_data_params(),
-                         save_dir=save_dir)
     
     def on_request(self, request_body, mime_type='application/json'): 
 
