42c86b85e8f67f117d5f71b5b4e9ce8faf7f4089
Shaka Chen <scchen@libgirl.com>
2019-06-11 16:46:30 +0800

    reindent

--- dong_mnist_example/model/train/default_config.py
+++ dong_mnist_example/model/train/default_config.py
@@ -2,9 +2,9 @@ from collections import OrderedDict
 
 def get_train_config():
     return OrderedDict([("self.compile", (":optimizer",
-                                "adam",
-                                ":loss",
-                                "sparse_categorical_crossentropy",
-                                ":metrics",
-                                [ "accuracy" ])),
+                                          "adam",
+                                          ":loss",
+                                          "sparse_categorical_crossentropy",
+                                          ":metrics",
+                                          [ "accuracy" ])),
                         ("self.fit", (":epochs", 5))])
