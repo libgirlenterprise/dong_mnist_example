0391b1391b874082ebd9e3d89c38c5d96d82be3c
Shaka Chen <scchen@libgirl.com>
2019-06-13 01:13:35 +0800

    done multiple request_handler

--- dong_mnist_example/service/default.py
+++ dong_mnist_example/service/default.py
@@ -8,9 +8,14 @@ from dong_mnist_example.model.init.default_load import DefaultModelLoad
 
 class DefaultService(DefaultModelLoad, dong.framework.Service):
 
-    
-    def on_request(self, request_body, mime_type='application/json'): 
+    @dong.framework.request_handler
+    def normal(self, request_body, mime_type='application/json'): 
 
         data = json.loads(request_body)
         x = numpy.array(data) / 255.0
         return json.dumps(self.predict(x).tolist())
+
+    @dong.framework.request_handler
+    def hello(self, request_body, mime_type='application/json'): 
+
+        return json.dumps('hello')
