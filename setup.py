fab97a0ceaaee28cb1750c60bec6906527d7759b
Shaka Chen <scchen@libgirl.com>
2019-06-17 19:50:19 +0800

    bumped version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dong_mnist_example',
-      version='0.3',
+      version='0.4',
       description='none',
       license='apache2',
       install_requires=[
